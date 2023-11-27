c9f1b03896d3b06beebcf8510fa5bf02e251ee13
17Ashwanth <ashwanthcheleri@gmail.com>
2023-11-27 11:04:41 +0530

    history edit

--- src/Components/History.jsx
+++ src/Components/History.jsx
@@ -3,38 +3,43 @@
 
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import IMG from '../Assets/5331039.jpg'
+import IMG from '../Assets/v910-aew-061.jpg'
 import { Link } from 'react-router-dom'
 
 
 function History() {
   return (
     <div>
-        <h1 className="text-center text-white mt-4">Transaction History</h1>
         <div className="d-flex justify-content-evenly flex-column mt-5 container">
             <Row className=''>
                 <Col md={6}>
-                <img src={IMG} alt="" width={'70%'} />
+                <img src={IMG} alt="" width={'90%'} />
                 <div className="mt-5">
                     <div className="border w-75">
-                        <h3 className="text-white ms-2">
+                        <h3 className="text-white ms-2 p-3">
                             Total Expense : ₹
                         </h3>
                     </div>
-                </div>
-                </Col>
-                <Col md={6}>
-                    <h3 className='text-center text-white mb-4'>Watch History</h3>
-                   <div className='d-flex justify-content-center'>
+                    <div className="border w-75 mt-3">
+                        <h3 className="text-white ms-2 p-3">
+                            Total Balance : ₹
+                        </h3>
+                    </div>
+                    <div className='d-flex mt-3'>
                         <button className='btn btn-outline-danger align-items-center'>
                             <Link to={'/Home'} style={{textDecoration:'none',color:'white',fontSize:'20px'}}><i class="fa-solid fa-backward me-2"></i>Back to Home</Link>
                         </button>
                    </div>
+                </div>
+                </Col>
+                <Col md={6}>
+                <h1 className="text-center text-white mt-4 mb-4">Transaction History</h1>
+                   
         <table className='table m-5 mb-5 container'>
         <thead>
           <tr>
             <th >SL NO</th>
-            <th >ITEMS</th>
+            <th >CATEGORY</th>
             <th >AMOUNT</th>
             <th >DATE</th>
             <th >ACTION</th>
@@ -52,6 +57,7 @@ function History() {
         </tr>
         </tbody>
         </table>
+        
                      
                  </Col>
             </Row>
